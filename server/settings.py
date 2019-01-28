960808c13525f2d86265b44437301f5a6ee4ec86
Zack Parker <zparker@salesforce.com>
2019-01-27 22:43:33 -0800

    Fix config

--- server/settings.py
+++ server/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
