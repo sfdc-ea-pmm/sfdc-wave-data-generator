24075a4251dce84133387dd4e44e57cf8c0e9d14
Terrence Tse <ttse@salesforce.com>
2019-05-23 17:18:52 -0700

    Schedule b2b commerce

--- sched.py
+++ sched.py
@@ -1,6 +1,7 @@
 import fsl_data_gen
 import sales_data_gen
 import services_data_gen
+import B2B_commerce_gen
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
@@ -17,5 +18,9 @@ def scheduled_services_data_gen():
 @scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=11)
 def scheduled_fsl_data_gen():
     fsl_data_gen.run()
+
+@scheduler.scheduled_job('cron', day_of_week='mon-fri', hour=12)
+def scheduled_b2b_commerce_data_gen():
+    B2B_commerce_gen.run()
 
 scheduler.start()
