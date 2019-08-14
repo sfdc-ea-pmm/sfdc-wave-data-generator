a109d2cdf34ecb6f831835ad45833d61aee120ab
Oscar Arzamendia <oarzamendia@salesforce.com>
2019-08-14 10:20:19 -0400

    splitting files in S3 completed and working

--- fins_ido_wealth_gen.py
+++ fins_ido_wealth_gen.py
@@ -1,22 +1,23 @@
 import os
-from datetime import date
-
-from fins_ido_wealth import fins_ido_wealth_generator
+import boto3
 import split_data_file
+
+from datetime import date
+from fins_ido_wealth import fins_ido_wealth_generator
+
 
 def run():
     today = date.today()
+    config_path = 'fins_ido_wealth/config.json'
 
     if os.environ.get('WRITE_MODE') != 'S3':
         source_path = 'fins_ido_wealth/data/input/'
         output_path = 'fins_ido_wealth/data/output/latest/'
         archive_path = 'fins_ido_wealth/data/output/archive/{}/'.format(today.isoformat())
-        config_path = 'fins_ido_wealth/config.json'
     else:
         source_path = 'fins-ido-wealth/data/input/'
         output_path = 'fins-ido-wealth/data/output/latest/'
         archive_path = 'fins-ido-wealth/data/output/archive/{}/'.format(today.isoformat())
-        config_path = 'fins-ido-wealth/config.json'
 
     if os.environ.get('WRITE_MODE') != 'S3' and not os.path.exists(output_path):
         os.makedirs(output_path)
@@ -29,10 +30,25 @@ def run():
     fins_ido_wealth_generator.run(source_path, archive_path, config_path)
 
     suffix = '-dataPart-'
-    for f in os.listdir(output_path):
-        if suffix not in f:
-            r = split_data_file.split_if_needed(output_path, f, output_path, suffix, deleteSource=True)
-            print(r)
+    if os.environ.get('WRITE_MODE') != 'S3':
+        for f in os.listdir(output_path):
+            if suffix not in f:
+                r = split_data_file.split_if_needed(output_path, f, output_path, suffix, deleteSource=True)
+                print(r)
+    else:
+        client = boto3.client('s3')
+        paginator = client.get_paginator('list_objects_v2')
+        result = paginator.paginate(Bucket=os.environ.get('S3_BUCKET_NAME'),StartAfter=output_path)
+        for page in result:
+            if "Contents" in page:
+                for key in page["Contents"]:
+                    path_and_file = key["Key"].rsplit('/', 1)
+                    f = path_and_file[-1] # get just the name of the file
+                    if path_and_file[0] not in output_path:
+                        break
+                    if suffix not in f:
+                        r = split_data_file.split_if_needed(output_path, f, output_path, suffix, deleteSource=True)
+                break # no need to look further down in the directory structure
 
 if __name__ == "__main__":
     # execute only if running as a script
--- split_data_file.py
+++ split_data_file.py
@@ -1,44 +1,78 @@
 import sys, os
+import boto3
+import codecs
+import csv
+import io
 
 kilobytes = 1024
 megabytes = kilobytes * 1000
 chunksize = int(10 * megabytes)      # default: 10 MB
 fileSuffix = '-dataPart-'
+fromdir = '' 
 
 
 def split(fromdir, fromfile, todir, suffix='part', chunksize=chunksize, deleteSource=False): 
     delete_later = ''
     fname, fext = fromfile.split('.')
-    if not os.path.exists(todir):                  # caller handles errors
-        os.mkdir(todir)                            # make dir, read/write parts
-    else:
-        for fn in os.listdir(todir):            # delete any existing splitted files
-            if fromfile == fn:
-                if deleteSource: delete_later = os.path.join(todir, fn)
-            else:
-                if fname+suffix in fn:
-                    os.remove(os.path.join(todir, fn)) 
-    
-    partnum = 0
-    input = open(fromdir+fromfile, 'rb')                   # use binary mode on Windows
-    while 1:                                       # eof=empty string from read
-        chunk = input.read(chunksize)              # get next part <= chunksize
-        if not chunk: break
-        partnum  = partnum+1
-        filename = os.path.join(todir, ((fname+suffix+'%04d'+'.'+fext) % partnum))
-        fileobj  = open(filename, 'wb')
-        fileobj.write(chunk)
-        fileobj.close()                            # or simply open(  ).write(  )
-    input.close(  )
-    assert partnum <= 9999                         # join sort fails if 5 digits
-    if deleteSource and delete_later!='': 
-        os.remove(delete_later)
+
+    if os.environ.get('READ_MODE') == 'S3':
+        client = boto3.client('s3')
+        s3_bucket_name = os.environ.get('S3_BUCKET_NAME')
+        source_file_object = client.get_object(Bucket=s3_bucket_name, Key=fromdir+fromfile)
+        source_file_body = source_file_object['Body']
+        text_stream = codecs.getreader("utf-8")(source_file_body)
+        partnum = 0
+        while 1:                                       # eof=empty string from read
+            chunk = text_stream.read(chunksize)        # get next part <= chunksize
+            if not chunk: break
+            partnum  = partnum+1
+            filename = os.path.join(todir, ((fname+suffix+'%d'+'.'+fext) % partnum))
+            chunk2 = io.BytesIO(bytes(chunk, 'utf-8'))
+            client.upload_fileobj(chunk2, s3_bucket_name, filename)
+            client.put_object_acl(ACL='public-read', Bucket=s3_bucket_name, Key=filename)
+        text_stream.close(  )
+
+        if deleteSource:
+            client.delete_object(Bucket=s3_bucket_name, Key=fromdir+fromfile)
+
+    else: 
+        if not os.path.exists(todir):                  # caller handles errors
+            os.mkdir(todir)                            # make dir, read/write parts
+        else:
+            for fn in os.listdir(todir):               # delete any existing splitted files
+                if fromfile == fn:
+                    if deleteSource: delete_later = os.path.join(todir, fn)
+                else:
+                    if fname+suffix in fn:
+                        os.remove(os.path.join(todir, fn))
+        
+        partnum = 0
+        input = open(fromdir+fromfile, 'rb')           # use binary mode on Windows
+        while 1:                                       # eof=empty string from read
+            chunk = input.read(chunksize)              # get next part <= chunksize
+            if not chunk: break
+            partnum  = partnum+1
+            filename = os.path.join(todir, ((fname+suffix+'%d'+'.'+fext) % partnum))
+            fileobj  = open(filename, 'wb')
+            fileobj.write(chunk)
+            fileobj.close()                            # or simply open(  ).write(  )
+        input.close(  )
+        assert partnum <= 9999                         # join sort fails if 5 digits
+        if deleteSource and delete_later!='': 
+            os.remove(delete_later)
     return partnum
             
 def split_if_needed(fromdir, fromfile, todir, suffix='part', chunksize=chunksize, deleteSource=False):
     absfrom = fromdir+fromfile
     absto = todir
-    fsize = os.path.getsize(absfrom)
+    if os.environ.get('READ_MODE') != 'S3':
+        fsize = os.path.getsize(absfrom)
+    else:
+        client = boto3.client('s3')
+        s3_bucket_name = os.environ.get('S3_BUCKET_NAME')
+        source_file_object = client.get_object(Bucket=s3_bucket_name, Key=fromdir+fromfile)
+        fsize = source_file_object['ContentLength']
+
     if fsize > chunksize:
         print ('Splitting', absfrom, 'to', absto, 'by', chunksize)
         r = split(fromdir, fromfile, todir, suffix, chunksize, deleteSource)
@@ -49,23 +83,24 @@ def split_if_needed(fromdir, fromfile, todir, suffix='part', chunksize=chunksize
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] == '-help':
-        print('Use: split_data_file.py [file-to-split target-dir [chunksize-in-bytes]]')
+        print('Use: split_data_file.py [from-dir file-to-split target-dir [chunksize-in-bytes] [deleteSource=True/False]]')
     else:
         ## this section is used when calling as a script
         if len(sys.argv) == 1:
+            fromdir = ''
             fromfile = 'test.csv'
             todir = 'testoutput'
         elif len(sys.argv) < 5:
             fromfile, todir = sys.argv[1:3]           # args in cmdline
             if len(sys.argv) == 4: chunksize = int(sys.argv[3])
         ##
-        absfrom, absto = map(os.path.abspath, [fromfile, todir])
+        _, absto = map(os.path.abspath, [fromfile, todir])
 
         try:
-            parts = split_if_needed(fromfile, todir, fileSuffix, chunksize)
+            parts = split_if_needed(fromdir, fromfile, todir, fileSuffix, chunksize, deleteSource=True)
         except:
             print ('Error during split:')
-            print (sys.exc_type, sys.exc_value)
+            print (sys.stderr)
         else:
             if parts == 0:
                 print('File not splitted')
