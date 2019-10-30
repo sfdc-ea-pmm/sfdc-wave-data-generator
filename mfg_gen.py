eddb57940c4a5c1e22e82043bbd51f67fcbdac6d
Oscar Arzamendia <oarzamendia@salesforce.com>
2019-10-30 11:40:19 -0300

    removed splitting method

--- mfg_gen.py
+++ mfg_gen.py
@@ -28,28 +28,6 @@ def run():
     mfg_generator.run(source_path, output_path, config_path)
 
     
-    if False: # no splitting for now oz.
-        suffix = '-dataPart-'
-        if os.environ.get('WRITE_MODE') != 'S3':
-            for f in os.listdir(output_path):
-                if suffix not in f:
-                    r = split_data_file.split_if_needed(output_path, f, output_path, suffix, deleteSource=True)
-                    print(r)
-        else:
-            client = boto3.client('s3')
-            paginator = client.get_paginator('list_objects_v2')
-            result = paginator.paginate(Bucket=os.environ.get('S3_BUCKET_NAME'),StartAfter=output_path)
-            for page in result:
-                if "Contents" in page:
-                    for key in page["Contents"]:
-                        path_and_file = key["Key"].rsplit('/', 1)
-                        f = path_and_file[-1] # get just the name of the file
-                        if path_and_file[0] not in output_path:
-                            break
-                        if suffix not in f:
-                            r = split_data_file.split_if_needed(output_path, f, output_path, suffix, deleteSource=True)
-                    break # no need to look further down in the directory structure
-
 if __name__ == "__main__":
     # execute only if running as a script
     run()
