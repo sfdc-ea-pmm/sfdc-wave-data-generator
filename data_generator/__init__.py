6acc14d35a198758be34f5b23237d7015cfbe59a
Rodrigo Mercader <rodrigo.mercader@oktana.com>
2023-05-23 12:46:03 -0300

    Replaced collections with collections.abc

--- data_generator/__init__.py
+++ data_generator/__init__.py
@@ -11,7 +11,7 @@ import random
 import sys
 import warnings
 
-from collections import OrderedDict
+from collections.abc import OrderedDict
 from numpy import random
 
 logger = logging.getLogger(__name__)
